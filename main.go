4587b9e2201c61b809a7f0215563d29b775d35ef
sunjiangmeng <1327605995@qq.com>
2025-12-03 17:31:16 +0800

    update

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"image/color"
-
 	"github.com/sjm1327605995/tenon/react"
 	"github.com/sjm1327605995/tenon/react/api"
 	"github.com/sjm1327605995/tenon/react/api/styles"
 	"github.com/sjm1327605995/tenon/react/elements"
 	"github.com/sjm1327605995/tenon/react/yoga"
+	"image/color"
 )
 
 type Hello struct {
@@ -19,11 +18,12 @@ func NewHello() *Hello {
 
 func (h *Hello) Render() api.Node {
 	return elements.NewView().Style(
-		styles.NewStyle().BackgroundColor(color.NRGBA{G: 255, A: 255}).HeightPercent(100).WidthPercent(100).
+		styles.NewStyle().
+			BackgroundColor(color.NRGBA{G: 255, A: 255}).HeightPercent(100).WidthPercent(100).
 			JustifyContent(yoga.JustifyCenter).AlignItem(yoga.AlignCenter)).
 		Child(elements.NewImage().
 			Style(styles.NewStyle().WidthPercent(50).HeightPercent(50)).
-			Source("react.svg"))
+			Source("react.svg"), elements.NewText().Content("hello"))
 
 }
 
